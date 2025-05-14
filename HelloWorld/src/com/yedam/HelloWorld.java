6dd526d154e2f87aba8775e0f39d0b73c747e937
min-seo7 <mms1117777@gmail.com>
2025-05-14 10:46:54 +0900

    첫번째 릴리즈

---
+++ HelloWorld/src/com/yedam/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.yedam;
+//자바: 컴파일(***.java -> ***.class 이 실행됨.)
+
+public class HelloWorld { 
+	public static void main(String[]args) { //메인 메소드.
+		for (int i = 1; i <= 3; i++ ) {
+		System.out.println("Hello, World");
+		}
+	}
+}
