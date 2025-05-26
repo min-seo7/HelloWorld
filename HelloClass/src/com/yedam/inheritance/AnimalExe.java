982a58592363ccf8af167ee684607715381c8c5d
min-seo7 <mms1117777@gmail.com>
2025-05-26 23:26:54 +0900

    정리

--- HelloClass/src/com/yedam/inheritance/AnimalExe.java
+++ HelloClass/src/com/yedam/inheritance/AnimalExe.java
@@ -5,11 +5,11 @@ public class AnimalExe {
 		//추상클래스는 인스턴스 생성불가.
 		//Animal animal = new Animal();   //Cannot instantiate the type Animal 오류. 
 		
-		Animal a1 = new Bird();   //자식 -> 부모로 자동형변환 발생. 
+		Animal a1 = new Bird();   //자식이 부모로 자동형변환 발생되어, 부모에게 정의된 메소드만 사용가능. 부모타입의 참조변수에 자식클래스 겍체할당.
 		a1.breathe();
 		a1.sound();		
 		
-		a1 = new Fish();
+		a1 = new Fish();  //부모클래스 타입의 참조변수라 상속받은 자식은 다 변수에 할당가능. 만약 배열을 부모타입 객체로 선언하게되면 자식객체 다 들어갈 수 있음. (다형성)
 		a1.breathe();
 		a1.sound();	
 		
