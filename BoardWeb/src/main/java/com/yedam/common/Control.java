1aca20646ac3a2b4d61034ba447fc76c460c4c0c
min-seo7 <mms1117777@gmail.com>
2025-06-13 17:48:22 +0900

    0613

--- BoardWeb/src/main/java/com/yedam/common/Control.java
+++ BoardWeb/src/main/java/com/yedam/common/Control.java
@@ -11,8 +11,9 @@ public interface Control {
 			throws ServletException, IOException;
 }
 
-ajax코드 교수님꺼 다시 확인할 것! 
-mapper도 확인
-
-
-promise, fetch[then-then-catch]
+/*
+ * ajax코드 교수님꺼 다시 확인할 것! mapper도 확인
+ * 
+ * 
+ * promise, fetch[then-then-catch]
+ */
